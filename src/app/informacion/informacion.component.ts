6015527f2a6ad9e438db5ca779898a026da73a1b
mga1398 <martingalvez2@gmail.com>
2019-08-30 15:21:09 -0500

    Actualizacion formulario reactivo

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-informacion',
@@ -6,12 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informacion.component.scss']
 })
 export class InformacionComponent implements OnInit {
+  pacienteForm = this.fb.group({
+    primerNombre: ['', Validators.required],
+    segundoNombre: [''],
+    primerApellido: ['', Validators.required],
+    segundoApellido: [''],
+    masinfo: this.fb.group({
+      cedula: ['', Validators.required],
+      nacimiento: [''],
+      genero: [''],
+    }),
+    contacto: this.fb.group({
+      pais: [''],
+      ciudad: [''],
+      correo: [''],
+      celular: [''],
+    }),
+  });
 
   selected = '+593';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.pacienteForm.value);
   }
 
 }
