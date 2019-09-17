ee1cf819f33a9a486c26d5f8515c8e2eea6ebb28
mga1398 <martingalvez2@gmail.com>
2019-09-17 16:20:04 -0500

    Actualizacion dialog citas

--- src/app/agendar-cita/nueva-cita.ts
+++ src/app/agendar-cita/nueva-cita.ts
@@ -3,6 +3,10 @@ import {Inject, Injectable} from '@angular/core';
 
 import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material';
 
+export interface Consultorio {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-agendar-cita',
@@ -11,6 +15,12 @@ import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material';
 })
 // tslint:disable-next-line: component-class-suffix
 export class NuevaCita {
+
+  public consultorios: Consultorio[] = [
+    {value: 'centrimed', viewValue: 'Centro Médico Centrimed'},
+    {value: 'metropolitano', viewValue: 'Hospital Metropolitano'},
+    {value: 'citimed', viewValue: 'Centro Médico Citimed'}
+  ];
   constructor(private  dialogRef: MatDialogRef<NuevaCita>, @Inject(MAT_DIALOG_DATA) public  data: any) {
   }
   public  closeMe() {
